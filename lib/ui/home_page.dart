97c0dfdcf46491cf161bc395a4623808cea1c860
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-04-13 19:15:29 -0300

    criada lista de contatos

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -40,7 +42,64 @@ class _HomePageState extends State<HomePage> {
         padding: EdgeInsets.all(10),
         child: ListView.builder(
           itemCount: contacts.length,
-          itemBuilder: (context, index) {},
+          itemBuilder: (context, index) {
+            return _contactCard(context, index);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    Contact contact = contacts[index];
+
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contact.img != null
+                        ? FileImage(File(contact.img))
+                        : Image.asset("assets/images/person-sem-image.png"),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      contact.name ?? "",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      contact.email ?? "",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      contact.phone ?? "",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
